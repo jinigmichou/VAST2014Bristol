4b815ceb944c96b3bda2f677c080e555d39d6bd0
nicolaspelce <nicolaspelce@eduroam-173-011.nomadic.bris.ac.uk>
2015-06-09 13:21:13 +0100

    Lauching Project

---
+++ src/ui/HomePanel.java
@@ -0,0 +1,49 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class HomePanel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomePanel window = new HomePanel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public HomePanel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 640, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnHome = new JButton("Home");
+		btnHome.setBounds(0, 6, 81, 29);
+		frame.getContentPane().add(btnHome);
+	}
+
+}
