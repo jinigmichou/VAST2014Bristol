1b45324e1bb73e6867ad7d966fda2f19a0bea263
nicolaspelce <nicolaspelce@eduroam-175-028.nomadic.bris.ac.uk>
2015-06-24 11:00:03 +0100

    Other next

---
+++ src/ui/HomeView.java
@@ -0,0 +1,239 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.border.LineBorder;
+
+import persist.WriterCSV;
+import core.Operator;
+import core.Reader;
+import core.Writer;
+
+
+public class HomeView extends JPanel implements ActionListener{
+
+	private MainView frame;
+	private JTextField textFieldFilePath;
+
+
+	/**
+	 * Create the panel.
+	 */
+	public HomeView(MainView frame) {
+
+		this.frame = frame;
+		setBackground(Color.LIGHT_GRAY);
+		
+
+		setLayout(null);
+
+
+		JLabel lblFilePath = new JLabel("File path: ");
+		lblFilePath.setBounds(50, 55, 180, 16);
+		
+		add(lblFilePath);
+
+		textFieldFilePath = new JTextField();
+		textFieldFilePath.setBounds(120, 55, 180, 16);
+		add(textFieldFilePath);
+		textFieldFilePath.setColumns(10);
+
+		JButton btnBrowse = new JButton("Browse");
+		btnBrowse.setBounds(325, 56, 180, 16);
+		btnBrowse.addActionListener(this);
+		btnBrowse.setActionCommand("Browse");
+		add(btnBrowse);
+
+		
+
+		JButton btnCalculate = new JButton("Calculate journey");
+		btnCalculate.setBounds(50, 171, 180, 16);
+		btnCalculate.addActionListener(this);
+		btnCalculate.setActionCommand("Calculate");
+		add(btnCalculate);
+
+		JButton btnSortByJourney = new JButton("Sort by journey");
+		btnSortByJourney.setBounds(50, 140, 180, 16);
+		
+		btnSortByJourney.addActionListener(this);
+		btnSortByJourney.setActionCommand("SortByJourney");
+		add(btnSortByJourney);
+		
+		JButton btnReturn = new JButton("Return to data type");
+		btnReturn.addActionListener(this);
+		btnReturn.setActionCommand("Return");
+		btnReturn.setBounds(50, 199, 180, 16);
+		add(btnReturn);
+		
+		JButton btnSort = new JButton("Sort");
+		btnSort.setBounds(50, 112, 180, 16);
+		btnSort.addActionListener(this);
+		btnSort.setActionCommand("Sort");
+		add(btnSort);
+		
+		JButton btnLogAndDependencies = new JButton("log and dependencies");
+		btnLogAndDependencies.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				frame.changePanel(new LogView(frame));
+				
+				
+			}
+		});
+		btnLogAndDependencies.setBounds(50, 227, 117, 29);
+		add(btnLogAndDependencies);
+
+
+		setVisible(true);
+	}
+
+
+
+	public String selectFile(){
+		String file= new String();
+		// Exemple numéro 2
+		// Boîte de sélection de fichier à partir du répertoire courant
+		File repertoireCourant = null;
+		try {
+			// obtention d'un objet File qui désigne le répertoire courant. Le
+			// "getCanonicalFile" n'est pas absolument nécessaire mais permet
+			// d'éviter les /Truc/./Chose/ ...
+			repertoireCourant = new File(".").getCanonicalFile();
+			//System.out.println("Répertoire courant : " + repertoireCourant);
+		} catch(IOException e) {}
+
+		// création de la boîte de dialogue dans ce répertoire courant
+		// (ou dans "home" s'il y a eu une erreur d'entrée/sortie, auquel
+		// cas repertoireCourant vaut null)
+		JFileChooser dialogue = new JFileChooser(repertoireCourant);
+
+		// affichage
+		dialogue.showOpenDialog(null);
+
+		// récupération du fichier sélectionné
+		//System.out.println("Fichier choisi : " + dialogue.getSelectedFile());
+		file= dialogue.getSelectedFile().toString();
+		return file;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String cmd = e.getActionCommand();
+		if(cmd.equals("Browse")){ 
+			textFieldFilePath.setText(this.selectFile());
+			String recup;
+			recup = textFieldFilePath.getText();
+
+			MainView.logger.log(Level.INFO, "Click on the buttom Browse, choice of the file "+ recup);
+
+
+		}
+		else if(cmd.equals("Sort")){ 
+
+			ArrayList<String[]> test2 = new ArrayList<String[]>();
+			String filePath= textFieldFilePath.getText();
+			MainView.logger.log(Level.INFO, "Click on the buttom Sort, it sorted the file " + filePath);
+
+			Reader myreader= new Reader(filePath);
+
+			String result = "" ;
+
+			try {
+
+
+				test2=myreader.readCsv(myreader.getMyFilePath());
+				for (int i= 0; i<test2.get(0).length; i++){
+					result = result + test2.get(0)[i] + ", ";
+					//comboBoxColumnName.addItem(test2.get(0)[i]);
+				}
+
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			MainView.logger.log(Level.INFO, "In the file there are " + result + "as data");
+			
+			frame.changePanel(new SortView(frame,test2.get(0), test2));
+
+
+		}
+		else if(cmd.equals("SortByJourney")){
+
+			ArrayList<String[]> test2 = new ArrayList<String[]>();
+			String filePath= textFieldFilePath.getText();
+
+						
+			MainView.logger.log(Level.INFO, "Click on the buttom SortByJourney, choice of the file "+ filePath);
+			
+
+			Reader myreader= new Reader(filePath);
+
+			try {
+
+
+				test2=myreader.readCsv(myreader.getMyFilePath());
+
+
+
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			
+
+			frame.changePanel(new SortByJourneyView(frame,test2));
+
+		}
+		else if(cmd.equals("Calculate")){
+
+			ArrayList<String[]> test2 = new ArrayList<String[]>();
+			String filePath= textFieldFilePath.getText();
+
+			Reader myreader= new Reader(filePath);
+
+			try {
+
+
+				test2=myreader.readCsv(myreader.getMyFilePath());
+				for (int i= 0; i<test2.get(0).length; i++){
+					System.out.println(test2.get(0)[i]);
+					//comboBoxColumnName.addItem(test2.get(0)[i]);
+				}
+
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			
+			frame.changePanel(new CalculJourneyView(frame, test2.get(0), test2));
+			
+		
+
+		}
+		else if(cmd.equals("Return")){
+			MainView frame = new MainView(0);
+			frame.changePanel(new ChooseDataSourceView(frame));
+			setVisible(true);
+		}
+
+
+
+
+	}
+}
---
+++ src/ui/MainView.java
@@ -0,0 +1,201 @@
+package ui;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileFilter;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.*;
+
+
+@SuppressWarnings("serial")
+public class MainView extends JFrame implements ActionListener {
+
+	//persist choosen
+	//private int persistType;
+
+	private static JPanel panelDisplay;
+	private SpringLayout springLayout;
+
+	private ChooseDataSourceView chooseDataSourceView;
+	private Test testPanel;
+
+	protected static Logger logger=
+			Logger.getLogger("ui.MainView");
+
+
+
+	//private AccountView accountPanel;
+
+	/**
+	 * Create the application.
+	 */
+	public static void main(String[] args){
+		new MainView(1);
+	}
+
+	public MainView(int log) {
+
+		//this.persistType = persistType;
+
+		if(log==1){
+			try {
+				File f = new File("src/logFiles/log");
+				//test if log exists, else we delete it
+				if ( f.exists() ) {
+					f.delete();
+				}
+				Handler fh = new FileHandler("src/logFiles/log", true);
+				fh.setFormatter(new SimpleFormatter());
+				logger.addHandler(fh);
+				logger.log(Level.INFO, "Start up of the program");
+
+
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		setTitle("Welcome");
+		setResizable(false);
+		initialize();
+	}
+
+	private void createWindow(String path){
+		JFrame f = new JFrame("Display the data file");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(new DisplayCsv(path));
+		f.setSize(new Dimension(700, 500));
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+	}
+
+	void chooseFile(){
+		JFileChooser jf = new JFileChooser();
+		jf.setFileFilter(new FileFilter() {
+			private String getExtension(File f) {
+				String ext = null;
+				String s = f.getName();
+				int i = s.lastIndexOf('.');
+
+				if (i > 0 &&  i < s.length() - 1) {
+					ext = s.substring(i+1).toLowerCase();
+				}
+				return ext;
+			}
+
+			@Override
+			public String getDescription() {
+				return "Files csv *.csv";
+			}
+
+			@Override
+			public boolean accept(File f) {
+				if (f.isDirectory()) {
+					return true;
+				}
+				String extension = getExtension(f);
+				if (extension != null) {
+					if (extension.equals("csv")){
+						return true;
+					} 
+					else {
+						return false;
+					}
+				}
+				return false;
+			}
+		});
+		int returnVal = jf.showOpenDialog(null);
+		if(returnVal==JFileChooser.APPROVE_OPTION){
+			createWindow(jf.getSelectedFile().getPath());
+		
+		}
+	}
+
+	void applyLookAndFeel(){
+		try{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}
+		catch(Exception e){}
+	}
+
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setBounds(100, 100, 800, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		springLayout = new SpringLayout();
+		getContentPane().setLayout(springLayout);
+
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		panelDisplay = new JPanel();
+		panelDisplay.setBackground(Color.LIGHT_GRAY);
+		springLayout.putConstraint(SpringLayout.NORTH, panelDisplay, 169, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, panelDisplay, 10, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panelDisplay, -10, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, panelDisplay, -10, SpringLayout.EAST, getContentPane());
+		getContentPane().add(panelDisplay);
+
+		JButton homeButton = new JButton("Click to Start");
+		chooseDataSourceView = new ChooseDataSourceView(this);
+		springLayout.putConstraint(SpringLayout.NORTH, homeButton, 10, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, homeButton, 10, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, homeButton, -613, SpringLayout.EAST, getContentPane());
+		homeButton.addActionListener(this);
+		homeButton.setActionCommand("Manage DB");
+		getContentPane().add(homeButton);
+
+
+
+		setVisible(true);
+
+	}
+
+	/**
+	 * Methode qui change le panel en fonction des actions
+	 * @param panel
+	 */
+
+	public void changePanel(JPanel panel){
+		//remove ancient JPanel if exist
+		/*panelDisplay.removeAll();
+		panelDisplay.repaint();
+		panelDisplay.revalidate();*/
+		//add the new JPanel
+		/*panelDisplay.add(panel);
+		panelDisplay.repaint();
+		panelDisplay.revalidate();*/
+
+		this.setContentPane(panel);
+		setVisible(true);	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		String cmd = e.getActionCommand();
+		if(cmd.equals("Manage DB")){ 
+			logger.log(Level.INFO, "Click on the buttom start application");
+			changePanel(new ChooseDataSourceView(this));
+			//setVisible(true);
+
+	}
+
+	}
+
+}
